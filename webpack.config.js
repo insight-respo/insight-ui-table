260197dacf3cb94783f6ad4a9bbcc7e632197676
jiangxuan <jiangxuan@dxy.cn>
2019-07-04 19:33:41 +0800

    feat: 完成配置, template 节点

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,36 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+if (
+  process.env.NODE_ENV !== 'development' &&
+  process.env.NODE_ENV !== 'production'
+) {
+  throw new Error('未知的 process.env.NODE_ENV ->', process.env.NODE_ENV);
+}
+
+const plugins = [
+  new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './index.html'),
+    }),
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+  );
+}
+
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV,
   devtool: 'source-map',
   entry: path.resolve(__dirname, './src/index.jsx'),
   output: {
     path: path.resolve(__dirname, './dist'),
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 9000,
   },
   module: {
     rules: [
@@ -34,9 +58,5 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './index.html'),
-    }),
-  ],
+  plugins,
 }
